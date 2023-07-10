b15f4c70a859a03b577c36fd998ad208e81e2795
Devrshi Bhavsar <132130415+ig-devrshi@users.noreply.github.com>
2023-07-10 22:30:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
+function reload() {
+    window.location.reload();
+}
 
 //IMAGE 1
-var random = Math.floor(Math.random() * 6)+1;
-var randomDiceImage = "dice"+ random +".png";
-var randomImageSource = "./images/"+randomDiceImage;
+var random = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + random + ".png";
+var randomImageSource = "./images/" + randomDiceImage;
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomImageSource);
+image1.setAttribute("src", randomImageSource);
 
 
 //IMAGE2
-var random2 = Math.floor(Math.random()*6)+1;
-var randomImageSource2 = "./images/dice"+random2+".png";
-var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+var random2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "./images/dice" + random2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-if(random>random2){
-    document.querySelector("h1").innerHTML=("Player 1 Wins!!");
+if (random > random2) {
+    document.querySelector("h1").innerHTML = ("Player 1 Wins!!");
 }
-else if(random<random2){
-    document.querySelector("h1").innerHTML=("Player 2 Wins!!");
+else if (random < random2) {
+    document.querySelector("h1").innerHTML = ("Player 2 Wins!!");
 }
 else {
-    document.querySelector("h1").innerHTML=("The Game is tied!");
+    document.querySelector("h1").innerHTML = ("The Game is tied!");
 }
 
-function reload(){
-    window.location.reload();
-}
+
